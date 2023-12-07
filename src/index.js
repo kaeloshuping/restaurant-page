989dc5285dcaa1e449b2ced12169508456503cd6
Kaelo Shuping <122022296+kaeloshuping@users.noreply.github.com>
2023-12-07 19:54:50 +0200

    added page header and footer to main page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
-import { loadPage } from "./load-page";
-import { header } from "./header";
+import { LoadPage } from "./load-page";
+import { header, footer } from "./header";
+import { homePage } from "./home-page";
 
+// create main page container
 const container = document.getElementById("content");
 
+// create new "LoadPage" object from LoadPage class
+let page = new LoadPage(homePage());
+
+// append web page header, content and footer to main page container
 container.appendChild(header());
+container.appendChild(page.currentPage());
+container.appendChild(footer());

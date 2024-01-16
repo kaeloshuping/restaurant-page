4c7a6101710a3ea5bacad4734d6568ff0e08c2d3
Kaelo Shuping <122022296+kaeloshuping@users.noreply.github.com>
2024-01-16 17:32:47 +0200

    added event listener to change page content when clicking on header tabs

--- src/index.js
+++ src/index.js
@@ -1,14 +1,37 @@
 import { LoadPage } from "./load-page";
 import { header, footer } from "./header";
 import { homePage } from "./home-page";
+import { aboutPage } from "./about";
 
 // create main page container
-const container = document.getElementById("content");
+const mainContainer = document.getElementById("content");
 
 // create new "LoadPage" object from LoadPage class
-let page = new LoadPage(homePage());
+let page = new LoadPage();
+
+page.currentPage = homePage();
+
+let homePageContainer = page.currentPage;
 
 // append web page header, content and footer to main page container
-container.appendChild(header());
-container.appendChild(page.currentPage());
-container.appendChild(footer());
+mainContainer.appendChild(header());
+mainContainer.appendChild(homePageContainer);
+mainContainer.appendChild(footer());
+
+let tabs = document.querySelectorAll(".header-tabs");
+
+// add event listener to header tabs to change page content upon clicking
+tabs.forEach( (tab) => {
+    tab.addEventListener("click", () => {
+        console.log(tab.id);
+        if (tab.id === "about") {
+            console.log("About tab is Clicked!");
+            page.currentPage = aboutPage();
+            let aboutPageContainer = page.currentPage;
+            console.log(aboutPageContainer.id);
+            mainContainer.replaceChild(aboutPageContainer, homePageContainer);
+        };
+    });
+});
+
+function removeChildNodes(container) {}
